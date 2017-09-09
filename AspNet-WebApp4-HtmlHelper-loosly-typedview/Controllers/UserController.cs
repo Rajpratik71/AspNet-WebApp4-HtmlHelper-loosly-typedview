6ced3860ee0e7d98193f06484b2249a57a9ac7c4
Pratik raj <rajpratik71@gmail.com>
2017-09-09 09:33:53 -0700

    Added user defined controller

---
+++ AspNet-WebApp4-HtmlHelper-loosly-typedview/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNet_WebApp4_HtmlHelper_loosly_typedview.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
