d1f66d35d54c87451cde04ba0706870bead3dd23
p <p@DESKTOP-FH2H5NO>
2017-09-09 09:25:51 -0700

    Add project files.

---
+++ AspNet-WebApp4-HtmlHelper-loosly-typedview/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNet_WebApp4_HtmlHelper_loosly_typedview
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspNet-WebApp4-HtmlHelper-loosly-typedview/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNet_WebApp4_HtmlHelper_loosly_typedview.Startup))]
+namespace AspNet_WebApp4_HtmlHelper_loosly_typedview
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
